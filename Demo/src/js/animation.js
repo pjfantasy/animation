218d08810b0f5cbde861da5d5ede40c83073ae56
Aries <liem.buu@PC-LIEMBUU>
2018-12-03 17:13:06 +0700

    my second commit

--- Demo/src/js/animation.js
+++ Demo/src/js/animation.js
@@ -1,12 +1,43 @@
 var Animation = function () {
+    var animationIn = '';
+    var animationOut = '';
+    var animationEnd = (function (el) {
+        var animations = {
+            animation: 'animationend',
+            OAnimation: 'oAnimationEnd',
+            MozAnimation: 'mozAnimationEnd',
+            WebkitAnimation: 'webkitAnimationEnd',
+        };
 
-    function introduce(){
-        return console.log('ahihi')
-    } 
+        for (var t in animations) {
+            if (el.style[t] !== undefined) {
+                return animations[t];
+            }
+        }
+    })(document.createElement('div'));
 
-    return { 
-        init: function () { 
-            introduce();
-        } 
-    } 
+    function AnimationStepEffect() {
+        $('.btn1').click(function () {
+            animationIn = 'fadeIn';
+            $('.step2').removeAttr("style").addClass('animated ' + animationIn);
+        })
+        $('.btn2').click(function () {
+            animationIn = 'bounceInDown';
+            $('.step3').removeAttr("style").addClass('animated ' + animationIn);
+        })
+        $('.btn3').click(function () {
+            animationIn = 'flash';
+            $('.step4').removeAttr("style").addClass('animated ' + animationIn);
+        })
+        $('.btn4').click(function () {
+            animationIn = 'fadeInLeft';
+            $('.step5').removeAttr("style").addClass('animated ' + animationIn);
+        })
+    }
+
+    return {
+        init: function () {
+            AnimationStepEffect();
+        }
+    }
 }();   
