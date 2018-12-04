af7255643c2116a8bf5cd4265b2e1d6a6b25ba74
Aries <liem.buu@PC-LIEMBUU>
2018-12-04 17:52:35 +0700

    third  commit
    add full animate css for demo
    complete UI: index.html

--- Demo/src/js/animation.js
+++ Demo/src/js/animation.js
@@ -1,4 +1,6 @@
 var Animation = function () {
+    var allAnimations = ["bounce","flash","pulse","rubberBand","shake","headShake","swing","tada","wobble","jello","bounceIn","bounceInDown","bounceInLeft","bounceInRight","bounceInUp","bounceOut","bounceOutDown","bounceOutLeft","bounceOutRight","bounceOutUp","fadeIn","fadeInDown","fadeInDownBig","fadeInLeft","fadeInLeftBig","fadeInRight","fadeInRightBig","fadeInUp","fadeInUpBig","fadeOut","fadeOutDown","fadeOutDownBig","fadeOutLeft","fadeOutLeftBig","fadeOutRight","fadeOutRightBig","fadeOutUp","fadeOutUpBig","flipInX","flipInY","flipOutX","flipOutY","lightSpeedIn","lightSpeedOut","rotateIn","rotateInDownLeft","rotateInDownRight","rotateInUpLeft","rotateInUpRight","rotateOut","rotateOutDownLeft","rotateOutDownRight","rotateOutUpLeft","rotateOutUpRight","hinge","jackInTheBox","rollIn","rollOut","zoomIn","zoomInDown","zoomInLeft","zoomInRight","zoomInUp","zoomOut","zoomOutDown","zoomOutLeft","zoomOutRight","zoomOutUp","slideInDown","slideInLeft","slideInRight","slideInUp","slideOutDown","slideOutLeft","slideOutRight","slideOutUp","heartBeat"]
+    var count = 0;
     var animationIn = '';
     var animationOut = '';
     var animationEnd = (function (el) {
@@ -30,11 +32,29 @@ var Animation = function () {
             $('.step4').removeAttr("style").addClass('animated ' + animationIn);
         })
         $('.btn4').click(function () {
-            animationIn = 'fadeInLeft';
-            $('.step5').removeAttr("style").addClass('animated ' + animationIn);
+            // allAnimations.forEach(function (animation, index){
+                // console.log(animation, index)
+                $('.step5').removeAttr("style").addClass('animated ' + allAnimations[count]);
+                $('h1.step5').html('<h1>Number ' + [count] + ': ' + allAnimations[count] + '</h1>');
+
+                count ++;
+            // })
+        })
+        $('.btn-input').click(function () {
+            animationIn = 'slideInUp';
+            if ($('#input1').valid()) {
+                $('.dropdown').removeAttr('style').addClass('animated ' + animationIn);
+            }
+        })
+        $('select').change(function () {
+            animationIn = 'zoomIn';
+                $('.radiobutton').removeAttr('style').addClass('animated ' + animationIn);
+        })
+        $("input[type=radio]").click(function () {
+            animationIn = 'flipInY';
+                $('.submit').removeAttr('style').addClass('animated ' + animationIn);
         })
     }
-
     return {
         init: function () {
             AnimationStepEffect();
